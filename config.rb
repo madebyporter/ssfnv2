fd4ad38b0fe11f90a145c35e684f37ec58f26481
madebyporter <general@madebyporter.com>
2019-09-15 16:55:41 -0700

    first commit

---
+++ config.rb
@@ -0,0 +1,57 @@
+require 'slim'
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+set :css_dir, 'assets/css'
+set :js_dir, 'assets/js'
+set :images_dir, 'assets/img'
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
+#  which_fake_page: 'Rendering a fake page with a local variable' }
+
+###
+# Helpers
+###
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+activate :directory_indexes
+
+activate :blog do |blog|
+  blog.prefix = "journal"
+  blog.permalink = "{category}/{title}.html"
+  blog.layout = "journal_layout"
+  blog.summary_separator = /SPLIT_SUMMARY_BEFORE_THIS/
+end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
---
+++ source/assets/js/all.js
@@ -0,0 +1,75 @@
+//= require_tree .
+
+var js = js || {},
+  body = document.getElementsByTagName('body')[0];
+
+// Scripts
+js.main = {
+  init: function () {
+    // this.cookiesAlert();
+    this.externalLinks();
+    this.functionMenu();
+    this.waypointsHeader();
+    this.journalShare();
+  },
+  cookiesAlert: function() {
+    window.addEventListener("load", function(){
+      window.cookieconsent.initialise({
+        "content": {
+          "message": "This website uses cookies to ensure you get the best experience on our website.",
+          "href": "http://myfdtps.netlify.com/privacy-policy"
+        }
+      })});
+  },
+  externalLinks: function() {
+    function externalLinks() {
+      var anchors = document.querySelectorAll( 'a' );
+      for( var i = 0; i < anchors.length; i++ ) {
+        if ( anchors[i].hostname !== window.location.hostname ) {
+            anchors[i].setAttribute( 'target', '_blank' );
+        }
+      }
+    }
+    externalLinks();
+  },
+  functionMenu: function() {
+    $('#functionMenuTrigger').on("click", function(){
+      if($('body').hasClass('menuOpen')){
+        $('body').removeClass('menuOpen');
+      } else {
+        $('body').addClass('menuOpen');
+      }
+    });
+  },
+  waypointsHeader: function() {
+    var b = document.getElementsByTagName('body')[0];
+    var waypoint = new Waypoint({
+      element: document.getElementsByClassName("site-block")[1], 
+      handler: function(direction) { 
+        if (direction === 'down'){
+          b.classList.add("fold");
+        }
+      },
+      offset: '0'
+    });
+
+    var waypoint = new Waypoint({
+      element: document.getElementsByClassName("site-block")[1],
+      handler: function(direction) {
+        if (direction === 'up'){
+          b.classList.remove("fold");
+        }
+      },
+      offset: '0'
+    });
+  },
+  journalShare: function() {
+    $("#journal-share").jsSocials({
+      shares: ["twitter", "facebook", "linkedin", "email"],
+      showLabel: false,
+    });
+  },
+};
+document.addEventListener('DOMContentLoaded', function(){
+  js.main.init();
+});
