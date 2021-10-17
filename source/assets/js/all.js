b2dc6771dcfe4ea76308512543d833053ec55b19
madebyporter <general@madebyporter.com>
2021-10-16 22:03:49 -0700

    fix smooth scroll for ios

--- source/assets/js/all.js
+++ source/assets/js/all.js
@@ -11,6 +11,7 @@ js.main = {
     this.gsapPageLoad();
     this.waypointsHeader();
     this.wavypointHero();
+    this.smoothScroll();
   },
   cookiesAlert: function() {
     window.addEventListener("load", function(){
@@ -60,6 +61,44 @@ js.main = {
     });
     $grid.imagesLoaded().progress( function() {
       $grid.masonry('layout');
+    });
+  },
+  smoothScroll: function() {
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
     });
   },
   waypointsHeader: function() {
